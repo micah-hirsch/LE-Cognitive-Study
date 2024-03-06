1fdb452021728ff72cc5b3b4cda23c5a8902d064
Micah Hirsch <micahhirsch06@gmail.com>
2024-03-06 15:02:23 -0500

    cleaned cognitive scores

---
+++ S. Bikulcius Thesis/data_cleaning.R
@@ -0,0 +1,40 @@
+# Data Processing
+
+# Author: Micah E. Hirsch, M.S. (mhirsch@fsu.edu)
+
+# Data: 3/6/2024
+
+# Purpose: To load and format cognitive data and merge it with the pupil
+# data for S. Bikulcius' undegraduate thesis. 
+
+# Loading Needed Packages
+
+library(tidyverse)
+library(rio)
+library(janitor)
+
+# Setting Working Directory
+
+setwd("~/Documents/LE-Cognitive-Study/Data/Raw")
+
+# Loading in cognitive data
+
+cog_data <- rio::import("2024-02-26 16.32.44 Assessment Scores.csv") |>
+  janitor::clean_names() |>
+  dplyr::select(pin, inst, uncorrected_standard_score, age_corrected_standard_score) |>
+  dplyr::mutate(measure = case_when(grepl("working memory", inst, ignore.case = T) ~ "list_sort",
+                                    grepl("flanker", inst, ignore.case = T) ~ "flanker",
+                                    grepl("dimensional change", inst, ignore.case = T) ~ "card_sort",
+                                    TRUE ~ "pattern"), .after = inst) |>
+  dplyr::select(-inst) |>
+  tidyr::pivot_longer(cols = uncorrected_standard_score:age_corrected_standard_score,
+                      names_to = "type",
+                      values_to = "score") |>
+  dplyr::mutate(measure = ifelse(grepl("uncorrected", type, ignore.case = T), 
+                                  paste(measure, "u", sep = "_"), paste(measure, "c", sep = "_"))) |>
+  dplyr::select(-type) |>
+  tidyr::pivot_wider(names_from = measure,
+                     values_from = score) |>
+  dplyr::rename(subject = pin)
+
+
